322514c197ed78e5ddec6c3f022e28b3af692724
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:35:42 +0200

    update file

---
+++ SteamAccountSwitcher/MyTaskbarIcon.cs
@@ -0,0 +1,17 @@
+using H.NotifyIcon;
+
+namespace SteamAccountSwitcher
+{
+	public class MyTaskbarIcon : TaskbarIcon
+	{
+		public MyTaskbarIcon()
+		{
+			ForceCreate();
+		}
+
+		public void ShowRunningInTrayNotification()
+		{
+			ShowNotification("Running in the tray", "Click the icon to see your accounts");
+		}
+	}
+}
