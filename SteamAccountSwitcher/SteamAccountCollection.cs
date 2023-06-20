e0d491860689ab57af78a253b58e168c8d735205
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:35:45 +0200

    update file

---
+++ SteamAccountSwitcher/SteamAccountCollection.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using Gameloop.Vdf;
+
+namespace SteamAccountSwitcher
+{
+	public class SteamAccountCollection : ObservableCollection<SteamAccount>
+	{
+		private FileSystemWatcher _watcher;
+		private string _installDirectory;
+		private string _configDirectory;
+
+		/// <summary>
+		/// Sets the Steam installation directory to be used, and reloads the list of accounts.
+		/// </summary>
+		public void SetDirectory(string installDirectory)
+		{
+			if (string.IsNullOrEmpty(installDirectory) || !Directory.Exists(installDirectory))
+				throw new ArgumentException("The directory doesn't exist.", nameof(installDirectory));
+
+			_installDirectory = installDirectory;
+			_configDirectory = Path.Combine(installDirectory, "config");
+
+			// Reinitialize filesystem watcher with the new directory.
+			_watcher?.Dispose();
+			_watcher = new(_configDirectory, "loginusers.vdf")
+			{
+				EnableRaisingEvents = true
+			};
+			_watcher.Changed += OnFileChanged;
+
+			Reload();
+		}
+
+		/// <summary>
+		/// Reloads the list of accounts by reading from the Steam directory.
+		/// </summary>
+		public void Reload()
+		{
+			if (string.IsNullOrEmpty(_installDirectory) || !Directory.Exists(_installDirectory))
+				throw new InvalidOperationException("Can't reload; Steam directory doesn't exist!");
+
+			Clear();
+			foreach (var account in GetAccounts(_configDirectory))
+				Add(account);
+		}
+
+		private void OnFileChanged(object sender, FileSystemEventArgs e)
+		{
+			System.Windows.Application.Current.Dispatcher.Invoke(Reload);
+		}
+
+		private string GetLoginUsersVdfContent(string configDirectory)
+		{
+			var loginUsersVdfPath = Path.Combine(configDirectory, "loginusers.vdf");
+
+			var triesLeft = 10;
+			while (true)
+			{
+				try
+				{
+					return File.ReadAllText(loginUsersVdfPath);
+				}
+				catch (IOException)
+				{
+					// Keep trying to read file if it's locked by another process.
+					triesLeft--;
+
+					if (triesLeft == 0)
+					{
+						// Give up.
+						throw;
+					}
+
+					// Blocking the thread is not ideal, but it's a rare situation anyway.
+					System.Threading.Thread.Sleep(200);
+				}
+			}
+		}
+
+		private IEnumerable<SteamAccount> GetAccounts(string configDirectory)
+		{
+			dynamic loginUsers = VdfConvert.Deserialize(GetLoginUsersVdfContent(configDirectory));
+
+			foreach (var loginUser in loginUsers.Value)
+			{
+				yield return new()
+				{
+					ID = loginUser.Key,
+					Name = loginUser.Value.AccountName.Value,
+					Alias = loginUser.Value.PersonaName?.Value,
+				};
+			}
+		}
+	}
+}
