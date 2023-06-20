62af4a593d4c2b634a998b6bef16ccf1093e650b
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:35:41 +0200

    update file

---
+++ SteamAccountSwitcher/AppResources.xaml.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SteamAccountSwitcher
+{
+	public partial class AppResources : ResourceDictionary
+	{
+		public AppResources()
+		{
+			InitializeComponent();
+		}
+
+		private async void MenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			var menuItem = (MenuItem)sender;
+
+			if (menuItem.CommandParameter is string stringParameter)
+			{
+				if (stringParameter == "add-account")
+				{
+					await SteamClient.LogOut();
+				}
+				else if (stringParameter == "options")
+				{
+					var optionsDialog = Application.Current.Windows.OfType<Options>().FirstOrDefault() ?? new Options();
+
+					if (optionsDialog.IsVisible)
+					{
+						optionsDialog.Activate();
+						return;
+					}
+
+					optionsDialog.ShowDialog();
+				}
+				else if (stringParameter == "exit")
+				{
+					Application.Current.Shutdown();
+				}
+			}
+			else if (menuItem.CommandParameter is SteamAccount account)
+			{
+				menuItem.IsEnabled = false;
+				await SteamClient.LogIn(account);
+				menuItem.IsEnabled = true;
+			}
+			else
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
